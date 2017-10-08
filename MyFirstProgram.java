60a913b622ad5901198da6c1dfcfc0f6910c0755
slegacheva <32405438+slegacheva@users.noreply.github.com>
2017-10-07 19:19:04 -0700

    summary
    my first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,20 @@
+package newpackage;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println("My name is Svetlana.");
+		System.out.println("Now I'm writing my first program.");
+		System.out.println("This is my first experience in Java.");
+		System.out.println("I'm very proud that I can do that.");
+		System.out.println("Also I understand that I need to work so hard\n if I want to get Java skills.");
+		System.out.println("I need to read books about Java.");
+		System.out.println("I need to practice every day.");
+		System.out.println("I need to attend classes every week.");
+		System.out.println("I need to communicate with developers.");
+		System.out.println("I must do that!!!");
+
+	}
+
+}
